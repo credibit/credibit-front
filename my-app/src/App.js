07dafeb51635f62c0353f688bdd5b41444ba01a4
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-27 14:18:32 -0500

    fix lint issues

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './Header/Header.js';
 
 function App() {
   return (
-    <div className="App">
+    <div>
          <Header/>
     </div>
   );
--- my-app/src/Header/Header.js
+++ my-app/src/Header/Header.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import './Header.css';
-import { Button } from 'react-bootstrap';
 
 class Header extends React.Component {
     render(){
         return (
-            <header>
+            <header className="header-style">
                 <nav className = "navbar navbar-default">
                     <div className="container">
                         <div className="navbar-header-wp">
                             <div className="navbar-header">
-                                <div id="logo-image" className="p-5">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQldYYzl9Z4y5Q1vDezF1y4ARQdzbvTC5zb0Jgg-WPJ9ijsDugx"/>
-                                    <Button>Hello</Button>
+                                <div id="logo-image">
+                                    <img 
+                                    alt=''
+                                    className="logo-image"
+                                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQldYYzl9Z4y5Q1vDezF1y4ARQdzbvTC5zb0Jgg-WPJ9ijsDugx"/>
                                 </div>
                             </div>
                         </div>
