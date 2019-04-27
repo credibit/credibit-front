c3ea66029bb4f1627063f47cc2d97e6d57655e65
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-27 18:46:52 -0500

    add form

--- my-app/src/Form/Form.js
+++ my-app/src/Form/Form.js
@@ -2,15 +2,63 @@ import React from 'react';
 import '../Form/Form.css';
 
 class Form extends React.Component {
+    constructor() {
+        super();
+        this.state = { 
+            personal: true,
+            enterprise: false,
+            credit: false,
+        }
+    }
+
+    handlePersonalClick = () => {
+        this.setState({
+          personal: true,
+          enterprise: false,
+          credit: false
+        })
+    }
+
+    handleEnterpriseClick = () => {
+        this.setState({
+          personal: false,
+          enterprise: true,
+          credit: false
+        })
+    }
+
+    handleCreditClick = () => {
+        this.setState({
+          personal: false,
+          enterprise: false,
+          credit: true
+        })
+    }
+
     render() {
         return(
             <div className="contain-form">
                 <div className="popup-form">
                     <div className="title">
                         Solicitar Credito
+                        <div>
+                            <ul className="nav-form">
+                                <li className={`${this.state.personal ? 'active' : 'non-active'}`}>
+                                
+                                    Personal
+                                </li>
+                                <li className={`${this.state.enterprise ? 'active' : 'non-active'}`}>
+                                    Empresa
+                                </li>
+                                <li className={`${this.state.credit ? 'active' : 'non-active'}`}>
+                                    Credito
+                                </li>
+                            </ul>
+                        <div/>
                     </div>
                 </div>
             </div>
+        </div>
         )
     }
 }
