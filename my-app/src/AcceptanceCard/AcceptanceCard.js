091966862af120c16cd5cbce63f9fff763444fd3
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-27 23:14:51 -0500

    acceptance card

---
+++ my-app/src/AcceptanceCard/AcceptanceCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './AcceptanceCard.css';
+
+class AcceptanceCard extends React.Component {
+    render() {
+        return(
+        <div className="acceptance-container">
+            <div className="title">
+            HELLO there
+            </div>
+        </div>
+        )
+    }
+
+}
+
+export default AcceptanceCard;
--- my-app/src/Result/Result.js
+++ my-app/src/Result/Result.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import InformationCard from '../InformationCard/InformationCard.js';
+import AcceptanceCard from '../AcceptanceCard/AcceptanceCard.js';
 import './Result.css';
 
 class Result extends React.Component {
     render() {
         return (
-            <div className="result-container">
-                <InformationCard/>
-                <InformationCard/>
-                <InformationCard/>
-                <InformationCard/>
-                <InformationCard/>
+            <div className="container-result">
+                <AcceptanceCard/>
+                <div className="result-container">
+                    <InformationCard
+                    icon="far fa-thumbs-up"
+                    title="Hi"
+                    description="Hello there"
+                    />
+                    <InformationCard
+                    icon="far fa-thumbs-up"
+                    title="Hi"
+                    description="Hello there"
+                    />
+                    <InformationCard
+                    icon="far fa-thumbs-up"
+                    title="Hi"
+                    description="Hello there"
+                    />
+                    <InformationCard
+                    icon="far fa-thumbs-up"
+                    title="Hi"
+                    description="Hello there"
+                    />
+                </div>
             </div>
         )
     }
