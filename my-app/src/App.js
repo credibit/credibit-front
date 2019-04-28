c404fede85c21470a28acb3036e95bca506b8394
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-28 03:05:28 -0500

    admin Page Cards

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
          <Header/>
-         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"></link>
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossOrigin="anonymous"></link>
     </div>
     
   );
