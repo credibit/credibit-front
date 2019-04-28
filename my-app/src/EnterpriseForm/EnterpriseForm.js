f64ac417b820191c51ae53f6f2060741247f7c2b
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-28 01:13:07 -0500

    slider

--- my-app/src/EnterpriseForm/EnterpriseForm.js
+++ my-app/src/EnterpriseForm/EnterpriseForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../EnterpriseForm/EnterpriseForm';
+import '../EnterpriseForm/EnterpriseForm.css';
 
 class EnterpriseForm extends React.Component{
     render() {
@@ -22,11 +22,14 @@ class EnterpriseForm extends React.Component{
                         </input>
                     </div>
                     <div>
-                        <i class="fas fa-building"></i>
-                        <input type="number" step="1" min="0" max="100" placeholder="Puntos SAT">
-                        </input>
+                        <i class="fas fa-building none-border"></i>
+                        <input type="range" class="custom-range" id="customRange1"/>
+                        <div className="border-separator"/>
+                        
                     </div>
                 </form>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+
             </div>
             
         )
