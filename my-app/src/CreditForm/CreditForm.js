561e0b9c73e06075308574198a3805be972597a3
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-27 19:50:58 -0500

    personal form

---
+++ my-app/src/CreditForm/CreditForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CreditForm extends React.Component{
+    render() {
+        return(
+            <div>
+                CREDIT
+            </div>
+        )
+    }
+
+}
+
+export default CreditForm;
